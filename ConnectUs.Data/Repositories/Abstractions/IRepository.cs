3f153b0c4230da34354afa133ae7f23be6890c5a
guzelberkay <guzelberkay@outlook.com>
2024-12-24 22:16:59 +0300

    IRepository and Repository clasess added

---
+++ ConnectUs.Data/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectUs.Data.Repositories.Abstractions
+{
+    public interface IRepository<T> where T : class , new()
+    {
+        Task AddAsync(T entity);
+
+        Task<List<T>> GetValuesAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+    }
+}
---
+++ ConnectUs.Data/Repositories/Concretes/Repository.cs
@@ -0,0 +1,54 @@
+﻿using ConnectUs.Data.Context;
+using ConnectUs.Data.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ConnectUs.Data.Repositories.Concretes
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly AppDbContext dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> Table
+        {
+            get => dbContext.Set<T>();
+        }
+
+        public async Task<List<T>> GetValuesAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table;
+
+            // Apply predicate if provided
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            // Apply include properties if provided
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
